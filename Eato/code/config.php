75654ed3393ec183364639a90633233c8ff4a1fc
Denver Hogan <hogandenver05@gmail.com>
2025-10-16 23:00:53 -0400

    use environment variable to store db password

--- Eato/code/config.php
+++ Eato/code/config.php
@@ -1,9 +1,13 @@
 <?php
-// Database connection for Eato
-$DB_HOST = 'localhost';
-$DB_NAME = 'eato';
-$DB_USER = 'root';
-$DB_PASS = '';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$DB_HOST = $_ENV['DB_HOST'] ?? 'localhost';
+$DB_NAME = $_ENV['DB_NAME'] ?? 'eato';
+$DB_USER = $_ENV['DB_USER'] ?? 'root';
+$DB_PASS = $_ENV['DB_PASS'] ?? '';
 
 try {
     $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8", $DB_USER, $DB_PASS);
