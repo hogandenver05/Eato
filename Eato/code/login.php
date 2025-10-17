23d3b449736926bd740acd757a905307317840e5
Denver Hogan <hogandenver05@gmail.com>
2025-10-17 19:56:59 -0400

    fix filepaths

--- Eato/code/login.php
+++ Eato/code/login.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 require_once 'config.php';
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 use \Firebase\JWT\JWT;
 
 $data = json_decode(file_get_contents('php://input'), true);
