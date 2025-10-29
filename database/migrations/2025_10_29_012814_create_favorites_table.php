499fc7c2542f063d3a788cc57ac454c7410101f1
Denver Hogan <hogandenver05@gmail.com>
2025-10-28 22:56:29 -0400

    migrate to Laravel

---
+++ database/migrations/2025_10_29_012814_create_favorites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id('favorite_id');
+            $table->foreignId('user_id')
+                ->constrained('users', 'id')
+                ->onDelete('cascade');
+            $table->foreignId('food_id')
+                ->constrained('foods', 'id')
+                ->onDelete('cascade');
+            $table->unique(['user_id', 'food_id']);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
