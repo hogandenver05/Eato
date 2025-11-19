1e68a162549c5a3e7a02f4189e987ad22e45aea4
Denver Hogan <hogandenver05@gmail.com>
2025-11-19 02:27:29 -0500

    Implement CRUD operations for favorites (#12)

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\FavoriteController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/foods', [FoodController::class, 'index']);
-    Route::post('/foods', [FoodController::class, 'store']);
-    Route::get('/foods/{food}', [FoodController::class, 'show']);
-    Route::put('/foods/{food}', [FoodController::class, 'update']);
-    Route::delete('/foods/{food}', [FoodController::class, 'destroy']);
+
+    Route::prefix('foods')->group(function () {
+        Route::get('/', [FoodController::class, 'index']);
+        Route::post('/', [FoodController::class, 'store']);
+        Route::get('{food}', [FoodController::class, 'show']);
+        Route::put('{food}', [FoodController::class, 'update']);
+        Route::delete('{food}', [FoodController::class, 'destroy']);
+    });
+
+    Route::prefix('favorites')->group(function () {
+        Route::get('/', [FavoriteController::class, 'index']);
+        Route::post('/', [FavoriteController::class, 'store']);
+        Route::delete('{favorite}', [FavoriteController::class, 'destroy']);
+    });
 });
