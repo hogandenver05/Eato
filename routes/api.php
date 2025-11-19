9227ff31506c351a794b11c5520ac4338641d70b
Denver Hogan <hogandenver05@gmail.com>
2025-11-18 22:06:41 -0500

    add Food routes to api router

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FoodController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/foods', [FoodController::class, 'index']);
+    Route::post('/foods', [FoodController::class, 'store']);
+    Route::get('/foods/{food}', [FoodController::class, 'show']);
+    Route::put('/foods/{food}', [FoodController::class, 'update']);
+    Route::delete('/foods/{food}', [FoodController::class, 'destroy']);
+});
