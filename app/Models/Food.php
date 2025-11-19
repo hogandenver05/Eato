80095d95502208ea0b7cdb8c850372f924c57487
Denver Hogan <hogandenver05@gmail.com>
2025-11-19 02:26:32 -0500

    Implement CRUD operations for favorites

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -10,6 +10,7 @@ class Food extends Model
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
 
+    protected $table = 'foods';
     protected $fillable = ['user_id', 'food_name', 'calories'];
 
     public function user()
